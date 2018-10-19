74b1173b1fa138fb47567aebd6fe09a24b40e5cf
Nixinerix <xoamara2@gmail.com>
2018-10-19 13:53:03 -0700

    JS outline and some html

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// Establish Variables
+
+let wins = 0;
+let losses = 0;
+let guessesLeft = 0;
+let compGuess = [];
+let userGuesses = [];
+
+// Computer picks random letter - Math.random then = to char?
+
+// onkeyup event for user
+    //userGuesses must be displayed until win or lose
+    //keyup value compared to compguess
+    //if true wins++, game restarts without page refresh
+    //if false guessesLeft--restarts without refresh
+
